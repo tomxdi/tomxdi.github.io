169e45bfe7ddb21cd1570d4cbdc9623e4e02ac53
tom <tom>
2023-03-07 15:31:31 -0600

    Update blog

--- try_openai.py
+++ try_openai.py
@@ -21,37 +21,47 @@ PATH_TO_BLOG_REPO=Path('/home/tom/projects/github.com/tomxdi/tomxdi.github.io/.g
 PATH_TO_XXX = Path('https://tomxdi.github.io/')
 
 FLAGS = flags.FLAGS
+flags.DEFINE_bool("init", False, "Do one time initialization")
+flags.DEFINE_bool("update", False, "Update the git blog")
 flags.DEFINE_string("topic", "Bike riding", "Specify the blog topic")
 #flags.DEFINE_bool("show", True, "Show files included zipfile")
 
+def update_blog(repo_path, commit_message="Update blog"):
+    print("Update blog repo {repo_path}")  
+
+    repo = Repo(repo_path)
+    
+    # git add .
+    repo.git.add(all=True)
+    # git commt -m "msg"
+    repo.index.commit(commit_message)
+    # git push
+    origin = repo.remote(name='origin')
+    origin.push()
+
+
 # absl needs argv
 def main(argv):
-    topic = FLAGS.topic
+    init = FLAGS.init
+    update = FLAGS.update
+    topic = FLAGS.topic    
     
     openai.api_key = os.getenv('OPENAI_API_KEY')
 
     PATH_TO_BLOG_REPO=Path('/home/tom/projects/github.com/tomxdi/tomxdi.github.io/.git')
     PATH_TO_BLOG = PATH_TO_BLOG_REPO.parent
     PATH_TO_CONTENT = PATH_TO_BLOG/"content"
-    print(PATH_TO_CONTENT)
-    PATH_TO_CONTENT.mkdir(exist_ok=True, parents=True)
-
-    def update_blog(commit_message="Update blog"):
-        repo = Repo(PATH_TO_BLOG_REPO)
-        # git add .
-        repo.git.add(all=True)
-        # git commt -m "msg"
-        repo.index.commit(commit_message)
-        # git push
-        origin = repo.remote(name='origin')
-        origin.push()
+    
+    if init:
+        PATH_TO_CONTENT.mkdir(exist_ok=True, parents=True)
 
     random_text_string = "xxxxxx"
 
     with open(PATH_TO_BLOG/"index.html", "w") as f:
         f.write(random_text_string)
     
-    update_blog()
+    if update:
+        update_blog(PATH_TO_BLOG_REPO)
     
 
     # Path('https://tomxdi.github.io/')
